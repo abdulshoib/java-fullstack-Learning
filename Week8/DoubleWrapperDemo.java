d7b3b9dbe69dd91eefc834d5c99e12c37a4382cf
abdulshoib <abdulshoib6281@gmail.com>
2026-07-29 23:31:54 +0530

    Week 8 Day 3: Added wrapper class programs

---
+++ Week8/DoubleWrapperDemo.java
@@ -0,0 +1,12 @@
+
+public class DoubleWrapperDemo {
+
+    public static void main(String[] args) {
+
+        Double price = Double.valueOf(1999.99);
+
+        System.out.println("Price : " + price);
+        System.out.println("Maximum : " + Double.MAX_VALUE);
+        System.out.println("Minimum : " + Double.MIN_VALUE);
+    }
+}
---
+++ Week8/IntegerWrapperDemo.java
@@ -0,0 +1,12 @@
+
+public class IntegerWrapperDemo {
+
+    public static void main(String[] args) {
+
+        Integer number = Integer.valueOf(100);
+
+        System.out.println("Value : " + number);
+        System.out.println("Maximum : " + Integer.MAX_VALUE);
+        System.out.println("Minimum : " + Integer.MIN_VALUE);
+    }
+}
